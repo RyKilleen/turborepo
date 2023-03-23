5582363f38865c235e211e375a3da245f13be9ea
Justin Ridgewell <justin@ridgewell.name>
2023-03-23 00:15:40 -0400

    Implement streamed static rendering

--- crates/turbopack-node/src/render/mod.rs
+++ crates/turbopack-node/src/render/mod.rs
@@ -1,7 +1,14 @@
+use async_stream::stream as generator;
 use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
+use turbo_tasks_bytes::Stream;
+use turbo_tasks_fs::FileSystemPathVc;
+use turbopack_core::asset::AssetVc;
+use turbopack_dev_server::source::Body;
 
-use crate::{route_matcher::Param, ResponseHeaders, StructuredError};
+use crate::{
+    pool::NodeJsOperation, route_matcher::Param, trace_stack, ResponseHeaders, StructuredError,
+};
 
 pub mod issue;
 pub mod node_api_source;
@@ -38,8 +45,6 @@ enum RenderProxyOutgoingMessage<'a> {
 #[serde(tag = "type", rename_all = "camelCase")]
 enum RenderProxyIncomingMessage {
     Headers { data: ResponseHeaders },
-    BodyChunk { data: Vec<u8> },
-    BodyEnd,
     Error(StructuredError),
 }
 
@@ -52,8 +57,60 @@ enum RenderStaticIncomingMessage {
         headers: Vec<(String, String)>,
         body: String,
     },
+    Headers {
+        data: ResponseHeaders,
+    },
     Rewrite {
         path: String,
     },
     Error(StructuredError),
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "camelCase")]
+enum RenderBodyChunks {
+    BodyChunk { data: Vec<u8> },
+    BodyEnd,
+    Error(StructuredError),
+}
+
+pub(crate) fn stream_body_chunks(
+    mut operation: NodeJsOperation,
+    intermediate_asset: AssetVc,
+    intermediate_output_path: FileSystemPathVc,
+) -> Body {
+    let chunks = Stream::new_open(
+        vec![],
+        Box::pin(generator! {
+            macro_rules! tri {
+                ($exp:expr) => {
+                    match $exp {
+                        Ok(v) => v,
+                        Err(e) => {
+                            operation.disallow_reuse();
+                            yield Err(e.into());
+                            return;
+                        }
+                    }
+                }
+            }
+
+            loop {
+                match tri!(operation.recv().await) {
+                    RenderBodyChunks::BodyChunk { data } => {
+                        yield Ok(data.into());
+                    }
+                    RenderBodyChunks::BodyEnd => break,
+                    RenderBodyChunks::Error(error) => {
+                        let trace =
+                            trace_stack(error, intermediate_asset, intermediate_output_path).await;
+                        let e = trace.map_or_else(Into::into, Into::into);
+                        yield Err(e);
+                        break;
+                    }
+                }
+            }
+        }),
+    );
+    Body::from_stream(chunks.read())
 }
